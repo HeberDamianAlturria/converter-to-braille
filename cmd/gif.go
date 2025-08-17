120b2ac9da009aa1e2f8541dbda4b5f14f7e5ad8
Heber Alturria <heber.alturria.git@gmail.com>
2025-08-17 15:22:41 -0300

    Feature: Add GIF command for converting GIF images to braille ASCII art

---
+++ cmd/gif.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"github.com/HeberDamianAlturria/converter-to-braille/internal/imageproc"
+	"github.com/HeberDamianAlturria/converter-to-braille/internal/loader"
+	"github.com/spf13/cobra"
+	"image"
+	"os"
+	"strings"
+	"time"
+)
+
+type GifOptions struct {
+    Inverted   bool
+    OutputPath string
+}
+
+var gifOpts GifOptions
+
+var gifCmd = &cobra.Command{
+	Use:   "gif [path]",
+	Short: "Convert an gif to braille",
+	Long:  "Convert an gif to braille ASCII art",
+	Args:  cobra.ExactArgs(1),
+	Run:   runGifCmd,
+}
+
+func runGifCmd(cmd *cobra.Command, args []string) {
+	path := args[0]
+
+	gif, err := loader.FromGifFile(path)
+	if err != nil {
+		cmd.PrintErrln("Error loading image:", err)
+		os.Exit(1)
+	}
+
+	for i, frame := range gif.Image {
+		img := image.Image(frame)
+
+		imgProc := imageproc.New(img)
+
+		var strBuilder strings.Builder
+
+		imgProc.WriteToBrille(&strBuilder, gifOpts.Inverted)
+
+		cmd.Print("\033[H\033[2J") // Clear terminal
+		
+		cmd.Println(strBuilder.String())
+
+		delay := time.Duration(gif.Delay[i]) * 10 * time.Millisecond
+
+		time.Sleep(delay)
+	}
+}
+
+func init() {
+	gifCmd.Flags().BoolVarP(&gifOpts.Inverted, "inverted", "i", false, "Invert braille colors")
+
+	rootCmd.AddCommand(gifCmd)
+}
